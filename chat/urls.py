8af2e6793eca1527367dd90d554c0d2f861e9d5c
dhimanparas20 <dhimanparas20@gmail.com>
2025-01-07 11:32:39 +0530

    implement User Registration API endpoint
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- chat/urls.py
+++ chat/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ChatRoomViewSet, MessageViewSet, ChatSessionViewSet
+from .views import ChatRoomViewSet, MessageViewSet, ChatSessionViewSet, UserViewSet
 
 # Initialize the router
 router = DefaultRouter()
 router.register(r'chatrooms', ChatRoomViewSet, basename='chatroom')
 router.register(r'chatrooms/(?P<chat_room_id>\d+)/messages', MessageViewSet, basename='message')
 router.register(r'chat_sessions', ChatSessionViewSet, basename='chat_session')
+router.register(r'user', UserViewSet, basename='userdet')
 
 # URLs for the app
 urlpatterns = [
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from .models import ChatRoom, Message, ChatSession
-from .serializers import ChatRoomSerializer, MessageSerializer, ChatSessionSerializer
+from django.contrib.auth.models import User
+from .serializers import ChatRoomSerializer, MessageSerializer, ChatSessionSerializer,UserSerializer
 
 class ChatRoomViewSet(viewsets.ModelViewSet):
     """ViewSet to list and create chat rooms."""
@@ -39,4 +40,29 @@ class ChatSessionViewSet(viewsets.ReadOnlyModelViewSet):
         # Fetch ChatSession instances related to the authenticated user
         user_sessions = self.get_queryset()
         serializer = self.get_serializer(user_sessions, many=True)
+        return Response(serializer.data)
+
+class UserViewSet(viewsets.ViewSet):
+    serializer_class = UserSerializer
+
+    def create(self, request):
+        """Handles user registration via POST."""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            password = request.data.get('password')
+            if not password:
+                return Response({"error": "Password is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+            user = User.objects.create_user(
+                username=serializer.validated_data['username'],
+                email=serializer.validated_data['email'],
+                password=password
+            )
+            return Response(self.serializer_class(user).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request):
+        permission_classes = [permissions.IsAuthenticated]
+        """Allows authenticated users to view their details via GET."""
+        serializer = self.serializer_class(request.user)
         return Response(serializer.data)
